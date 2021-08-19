9c4af3d555455bee4434f944c011c7825cf1d5ef
Kulakivsky <ruslankulakivsky@gmail.com>
2021-08-19 19:05:28 +0300

    Connected to database

--- demo/src/main/java/com/example/demo/SpringSecurityAmigosCodeApplication.java
+++ demo/src/main/java/com/example/demo/SpringSecurityAmigosCodeApplication.java
@@ -1,13 +1,34 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class SpringSecurityAmigosCodeApplication {
+public class SpringSecurityAmigosCodeApplication implements CommandLineRunner {
+
+	public SpringSecurityAmigosCodeApplication() {}
+
+	public SpringSecurityAmigosCodeApplication(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityAmigosCodeApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		String sql = "INSERT INTO students (name, email) VALUES ("
+				+ " 'Nam Ha Minh', 'nam@codejava.net')";
+
+			int rows = jdbcTemplate.update(sql);
+
+	}
 }
+
---
+++ demo/src/main/java/com/example/demo/datasource/PostgresDataSource.java
@@ -0,0 +1,3 @@
+package com.example.demo.datasource;
+
+public class PostgresDataSource { }
